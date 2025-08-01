f4c114679602514b4c9e9498b48300ae4d2e6b0b
James Garner <james.garner@canonical.com>
2025-08-01 17:59:02 +1200

    test: cleanup

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -42,47 +42,19 @@ def test_deploy_functional(juju: jubilant.Juju, charm_path: pathlib.Path):
         "microk8s",
         "kubectl",
         "apply",
-        # "-f",
-        # "tests/integration/ok-200.yaml",
         "-f",
         "tests/integration/pebble.yaml",
-        # "-f",
-        # "tests/integration/pebble-challtestsrv.yaml",
     ]
     try:
         subprocess.check_output(microk8s_cmd)
     except subprocess.CalledProcessError:
         subprocess.check_output(["sudo", *microk8s_cmd])
 
-    # WIP: expose pebble-challtestsrv management endpoint for configuration
-    # time.sleep(3)
-    # port_forward_cmd = [
-    #     "microk8s",
-    #     "kubectl",
-    #     "port-forward",
-    #     "svc/pebble-challtestsrv",
-    #     "--namespace",
-    #     "model",
-    #     "8055:8055",
-    # ]
-    # try:
-    #     subprocess.check_output(port_forward_cmd)
-    # except subprocess.CalledProcessError:
-    #     subprocess.check_output(["sudo", *port_forward_cmd])
-    # url = "http://localhost:8055/"
-
-    uri = juju.add_secret(
-        "plugin-config",
-        {
-            "httpreq-endpoint": "http://127.0.0.1:8080",  # DNS server
-            # "http01-iface": "http://pebble-challtestsrv",  # unfortunately no solver is detected for http-01, so we always fall back to dns-01
-            # "http01-port": "5002",
-        },
-    )
+    uri = juju.add_secret("plugin-config", {"httpreq-endpoint": "http://127.0.0.1:8080"})  # DNS
     config = {
         "email": "example@example.com",
-        "plugin": "httpreq",  # pylego readme indicates that "http" should also be a supported plugin, but it's not
-        "server": "https://pebble:14000/dir",  # ACME server
+        "plugin": "httpreq",
+        "server": "https://pebble:14000/dir",  # ACME
         "plugin-config-secret-id": uri.unique_identifier,
     }
     juju.deploy(charm_path, config=config)
@@ -102,20 +74,26 @@ def test_deploy_functional(juju: jubilant.Juju, charm_path: pathlib.Path):
     # deploy our challenge listener + dns resolver into the lego container
     juju.scp("tests/integration/server.py", "lego/0:/")
     juju.scp("tests/integration/requirements.txt", "lego/0:/")
-    juju.ssh(
-        "lego/0",
-        "cd / && curl -LsSf https://astral.sh/uv/install.sh | sh && /root/.local/bin/uv venv && /root/.local/bin/uv pip install -r requirements.txt && echo -e 'search model.svc.cluster.local svc.cluster.local cluster.local\nnameserver 127.0.0.1\noptions ndots:5' > /etc/resolv.conf",
-    )
-    # juju.ssh("lego/0", "curl -LsSf https://astral.sh/uv/install.sh | sh && uv pip nohup /root/.local/bin/uv run --script /server.py > /server.log2 2>&1 &")
-    # juju.ssh("lego/0", "echo -e 'search model.svc.cluster.local svc.cluster.local cluster.local\nnameserver 127.0.0.1\noptions ndots:5' > /etc/resolv.conf")
+    resolv_conf = """
+search model.svc.cluster.local
+nameserver 127.0.0.1
+options ndots:5
+""".strip()
+    ssh_cmds = [
+        "cd /",
+        "curl -LsSf https://astral.sh/uv/install.sh | sh",
+        "/root/.local/bin/uv venv",
+        "/root/.local/bin/uv pip install -r requirements.txt",
+        f"echo -e '{resolv_conf}' > /etc/resolv.conf",
+    ]
+    juju.ssh("lego/0", " && ".join(ssh_cmds))
     # juju.ssh uses subprocess with a ['list', 'of', 'args'], which mangles the quoting on our bash -c
-    subprocess.check_output(
-        "juju ssh lego/0 \"cd / && bash -c 'source .venv/bin/activate && nohup python3 server.py > /server.log2 2>&1 & sleep 1'\"",
-        shell=True,
-    )
-    # echo -e 'search model.svc.cluster.local svc.cluster.local cluster.local\nnameserver 10.152.183.10\noptions ndots:5' > /etc/resolv.conf
+    ssh_cmd = "cd / && bash -c 'source .venv/bin/activate && nohup python3 server.py > /server.log2 2>&1 & sleep 1'"
+    subprocess.check_output(f"juju ssh lego/0 \"{ssh_cmd}\"", shell=True)
 
+    # request certificate
     juju.integrate("lego", "tls-certificates-requirer")
-    # failure to acquire certificate doesn't show up in status, only in logs ...
+    # failure to acquire certificate doesn't show up in status, only in logs
     juju.wait(jubilant.all_active)
     # certificate fails to be acquired because pebble-challtestsrv doesn't appear to respond to anything
+    # currently not verified in this test
